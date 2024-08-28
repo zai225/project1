09ffedc7608204cdb8451015c04ed751b7598386
Zaid <zaidu2020@gmail.com>
2024-08-28 15:07:51 +0530

    Sum Calculator added

--- index.py
+++ index.py
@@ -1,2 +1,15 @@
 a = "Hello World"
 print(a)
+
+num1 = float(input("Enter your number: "))
+num2 = float(input("Enter your number: "))
+
+sum_result = num1 + num2
+difference_result = num1 - num2
+multiply_result = num1 * num2
+divisibily_result = num1 / num2
+
+print(f"sum: {sum_result}")
+print(f"difference: {difference_result}")
+print(f"multiply: {multiply_result}")
+print(f"divisible: {divisibily_result}")
