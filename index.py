caff66b2a5c79e2174a90e78c8dfa4935b3968e2
Zaid <zaidu2020@gmail.com>
2024-09-03 00:21:55 +0530

    updated verseron

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 import requests
 import json
 query = input("What type of news are you interested in? ")
-url = f"https://newsapi.org/v2/everything?q={query}&domains=techcrunch.com&from=2024-08-02&sortBy=publishedAt&apiKey=487fdc3a817b47629264ee425ffbab0b"
+url = f"https://newsapi.org/v2/everything?q={query}&from=2024-08-02&sortBy=publishedAt&apiKey=487fdc3a817b47629264ee425ffbab0b"
 r = requests.get(url)
 # print(r.text)
 # json.loads(r.text)
-a = json.loads(r.text)
-print(a["articles"][0])
+news = json.loads(r.text)
+for article in news["articles"]:
+  print(article["title"])
+  print(article["description"])
+  print("--------------------------------------")
